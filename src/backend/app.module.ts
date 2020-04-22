c380cc57d81babf9b9b48dc8d6721cf2c2f08c7d
Saikumar Vantaku <saikumar@everest.engineering>
2020-04-22 19:33:34 +0530

    Remove the sample code inserted initially

--- src/backend/app.module.ts
+++ src/backend/app.module.ts
@@ -2,12 +2,10 @@ import {Module} from '@nestjs/common';
 import {GraphQLModule} from '@nestjs/graphql';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
-import {NameModule} from './sample/name.module';
 import {TicketModule} from './ticket/ticket.module';
 
 @Module({
   imports: [
-    NameModule,
     TicketModule,
     GraphQLModule.forRoot({
       debug: false,
--- src/backend/sample/name.module.ts
+++
@@ -1,8 +0,0 @@
-import {Module} from '@nestjs/common';
-import {NameResolver} from './name.resolver';
-import {NameService} from './name.service';
-
-@Module({
-  providers: [NameResolver, NameService],
-})
-export class NameModule {}
